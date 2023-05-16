7bcc3145d73cead0a09c090a6e93e1a1ecb12c72
Chritelle-12 <mariechristellenirere@gmail.com>
2023-05-16 17:26:05 +0200

    Add edit button functionality

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import '../CSS/book.css';
 
 const Book = ({ book, deleteBookHandler, editBookHandler }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(book.title);
+  const [editedAuthor, setEditedAuthor] = useState(book.author);
+
   const handleDelete = () => {
     deleteBookHandler(book.item_id);
   };
 
   const handleEdit = () => {
-    editBookHandler(book.item_id);
+    if (isEditing) {
+      // Save changes
+      editBookHandler(book.item_id, editedTitle, editedAuthor);
+    }
+    setIsEditing(!isEditing);
+  };
+
+  const handleTitleChange = (e) => {
+    setEditedTitle(e.target.value);
+  };
+
+  const handleAuthorChange = (e) => {
+    setEditedAuthor(e.target.value);
   };
 
   return (
     <section className="wrapper">
-      <h2>{book.title}</h2>
-      <h3>{book.author}</h3>
+      <h2>
+        {isEditing ? (
+          <input type="text" value={editedTitle} onChange={handleTitleChange} />
+        ) : (
+          book.title
+        )}
+      </h2>
+      <h3>
+        {isEditing ? (
+          <input type="text" value={editedAuthor} onChange={handleAuthorChange} />
+        ) : (
+          book.author
+        )}
+      </h3>
       <p>{book.category}</p>
       <ul className="ul-but">
         <li>
@@ -22,7 +50,7 @@ const Book = ({ book, deleteBookHandler, editBookHandler }) => {
         </li>
         <li>
           <button type="button" onClick={handleEdit}>
-            Edit
+            {isEditing ? 'Save' : 'Edit'}
           </button>
         </li>
         <li>
